3d18088cb626d6dea3d8791c26fd3fbd3be26804
Andrey <slendernet2@gmail.com>
2018-10-12 14:01:32 +0300

    Hometask 3 quest 2

---
+++ Composition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT_4
+{
+    class Composition
+    {
+        public Flower[] _compositionOfFlowers;
+        public double compositionCost;
+        public Composition(params Flower [] composition)
+        {
+            _compositionOfFlowers = composition;
+        }
+        public double CompositionCost()
+        {
+            for (int i = 0; i < _compositionOfFlowers.Length; i++)
+            {
+                compositionCost += _compositionOfFlowers[i]._cost;
+            }
+            return compositionCost;
+        }
+        public string CompositionOfFlowers()
+        {
+            string composition = null;
+            for (int i = 0; i < _compositionOfFlowers.Length; i++)
+            {
+                composition += _compositionOfFlowers[i]._color + " " + _compositionOfFlowers[i]._name + " ";
+            }
+            return composition;
+        }
+    }
+}
---
+++ Flower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT_4
+{
+    class Flower
+    {
+        public string _name;
+        public string _color;
+        public int _cost;
+
+        public Flower(string name, string color, int cost)
+        {
+            _name = name;
+            _color = color;
+            _cost = cost;
+        }
+    }
+}
